318c9104e44440261551f8e336ba66fd08850fb9
Conrad Coyanda-Parkzes <conradnexus@gmail.com>
2024-07-05 19:19:03 +0000

    added page tests and github actions yaml file auto testing

---
+++ tests/test_users.py
@@ -0,0 +1,50 @@
+import unittest, sys, os
+
+sys.path.append('../week-3-day-2') # imports python file from parent directory
+from main import app, db
+
+class UsersTests(unittest.TestCase):
+
+    # executed prior to each test
+    def setUp(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+        self.app = app.test_client()
+        with app.app_context():
+            db.drop_all()
+            db.create_all()
+
+    ###############
+    #### tests ####
+    ###############
+
+    def register(self, username, email, password):
+        return self.app.post('/register',
+                            data=dict(username=username,
+                                      email=email,
+                                      password=password, 
+                                      confirm_password=password),
+                            follow_redirects=True)
+
+    def test_valid_user_registration(self):
+        response = self.register('test', 'test@example.com', 'FlaskIsAwesome')
+        self.assertEqual(response.status_code, 200)
+
+    def test_invalid_username_registration(self):
+        response = self.register('t', 'test@example.com', 'FlaskIsAwesome')
+        self.assertIn(b'Field must be between 2 and 20 characters long.', response.data)
+
+    def test_too_long_username_registration(self):
+        response = self.register('trefnuebrjbjsbjfbeubfuebjfbbvbbvjrebjvbejsbjfvbj', 'test@example.com', 'FlaskIsAwesome')
+        self.assertIn(b'Field must be between 2 and 20 characters long.', response.data)
+
+    def test_invalid_email_registration(self):
+        response = self.register('test2', 'test@example', 'FlaskIsAwesome')
+        self.assertIn(b'Invalid email address.', response.data)
+
+    def test_no_at_sign_email_registration(self):
+        response = self.register('test2', 'testexample.com', 'FlaskIsAwesome')
+        self.assertIn(b'Invalid email address.', response.data)
+
+
+if __name__ == "__main__":
+    unittest.main()
